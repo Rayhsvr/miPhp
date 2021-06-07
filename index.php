828debdcc6738e0d5bcbd86666887e0329f44226
Rayhsvr <rayhsvr@gmail.com>
2021-06-07 18:47:06 -0500

    Mi primer Script en php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Mi primer php</title>
+  </head>
+  <body>
+    <header>
+      <h1>Mi Primer Script en PHP</h1>
+    </header>
+    <nav>
+
+    </nav>
+    <section>
+    <?
+    $texto="Hola Mundo, ver 2.";
+    echo "<p>$texto</p>\n";
+    echo '<p>'.$texto.'</p>'."\n";
+    ?>
+  </section>
+  <footer>Derechos Reservador 2021</footer>
+  </body>
+</html>
