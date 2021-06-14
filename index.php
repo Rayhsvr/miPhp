9cb2fedbe1558689b64aca3038e58249c520a1e4
Rayhsvr <rayhsvr@gmail.com>
2021-06-14 18:24:40 -0500

    Versión con algunos cambios

--- index.php
+++ index.php
@@ -9,15 +9,15 @@
       <h1>Mi Primer Script en PHP</h1>
     </header>
     <nav>
-
+      <!--  Aqui va el menú -->
     </nav>
     <section>
-    <?
+    <?php
     $texto="Hola Mundo, ver 2.";
     echo "<p>$texto</p>\n";
     echo '<p>'.$texto.'</p>'."\n";
     ?>
   </section>
-  <footer>Derechos Reservador 2021</footer>
+  <footer>Derechos Reservados 2021</footer>
   </body>
 </html>
